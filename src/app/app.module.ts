83b26dd2f62382f463919b84b5390f5d0e08a5ab
mydemo <patilshruti1857@gmail.com>
2024-03-15 15:54:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneComponent } from './one/one.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatRadioModule} from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
+import { TwoComponent } from './two/two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatRadioModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync(),
+  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/two/two.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two',
+  templateUrl: './two.component.html',
+  styleUrl: './two.component.scss'
+})
+export class TwoComponent {
+  selectedValue:string='';
+
+  
+}
